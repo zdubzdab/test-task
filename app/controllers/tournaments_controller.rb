60de3df80d29410071b27af0d7ab944b9eefd9be
Volodymyr Dorotiuk <volodymyr.dorotiuk@onapp.com>
2022-11-14 19:47:25 +0200

    Add i18n and table with tournaments

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class TournamentsController < ApplicationController
-  def index; end
+  helper_method :tournaments
+
+  def index
+    tournaments
+  end
 
   def new; end
 
   def create; end
 
   def show; end
+
+  private
+
+    def tournaments
+      @tournaments ||= Tournament.all
+    end
 end
